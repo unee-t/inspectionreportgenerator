91483de6ab6e9d1113677ece2139e11c7cc63f94
Kai Hendry <kai.hendry@unee-t.com>
2019-01-08 10:17:36 +0800

    We need to feed the PDF generator the document_url from S3, else it will have old contents

--- regenerate.sh
+++ regenerate.sh
@@ -1,7 +1,17 @@
-#!/bin/bash
+#!/bin/bash -e
 INPUT=$1
 
-# aws --profile uneet-dev s3 ls --recursive s3://dev-media-unee-t/ | grep json$ | grep 2018-11-23 | while read -r _ _ _ fn; do echo s3://dev-media-unee-t/$fn; done
+# aws --profile $AWS_PROFILE s3 ls --recursive s3://dev-media-unee-t/ | grep json$ | grep 2018-11-23 | while read -r _ _ _ fn; do echo s3://dev-media-unee-t/$fn; done
+
+udomain() {
+	case $1 in
+		prod) echo $2.unee-t.com
+		;;
+		*) echo $2.$1.unee-t.com
+		;;
+	esac
+}
+
 
 if test "${INPUT##*.}" != "json"
 then
@@ -9,10 +19,27 @@ then
 	exit 1
 fi
 
-# INPUT=s3://dev-media-unee-t/2018-12-10/228-b1ed678a.json
+BUCKET=$(echo $INPUT | awk -F[/:] '{print $4}')
+
+case $BUCKET in
+	prod-media-unee-t)
+	STAGE=prod
+	;;
+	dev-media-unee-t)
+	STAGE=dev
+	;;
+	*)
+	echo Unknown bucket: $BUCKET
+	exit 1
+	;;
+esac
+
+
+AWS_PROFILE=uneet-$STAGE
+
 # step 1 download JSON
 
-aws --profile uneet-dev s3 cp $INPUT /tmp
+aws --profile $AWS_PROFILE s3 cp $INPUT /tmp
 
 # step 2 regenerate HTML file
 
@@ -22,12 +49,12 @@ echo Working on $fn
 cat <<< "$(jq ".force += true " < $fn)" > $fn
 
 HTML=$(curl -X POST \
-  https://pdfgen.dev.unee-t.com \
-  -H "Authorization: Bearer $(aws --profile uneet-dev ssm get-parameters --names API_ACCESS_TOKEN --with-decryption --query Parameters[0].Value --output text)" \
-  -H 'Content-Type: application/json' \
-  -H 'cache-control: no-cache' \
-  --data @$fn |
-  jq -r .HTML)
+	https://$(udomain $STAGE pdfgen) \
+	-H "Authorization: Bearer $(aws --profile $AWS_PROFILE ssm get-parameters --names API_ACCESS_TOKEN --with-decryption --query Parameters[0].Value --output text)" \
+	-H 'Content-Type: application/json' \
+	-H 'cache-control: no-cache' \
+	--data @$fn |
+	jq -r .HTML)
 
 echo New output: $HTML
 
@@ -38,9 +65,10 @@ echo Setting date: $date
 
 # document_url must be from the host unee-t.com btw
 curl -X POST \
-  https://prince.dev.unee-t.com \
-  -H 'Content-Type: application/json' \
-  -H 'cache-control: no-cache' \
-  -d "{ \"document_url\": \"$HTML\", \"date\": \"$date\"}"
+	https://$(udomain $STAGE prince) \
+	-H 'Content-Type: application/json' \
+	-H 'cache-control: no-cache' \
+	-d "{ \"document_url\": \"$HTML\", \"date\": \"$date\"}"
 
 #aws --profile uneet-dev cloudfront create-invalidation --distribution-id E2L4KVYCVKXLA1 --invalidation-batch "{ \"Paths\": { \"Quantity\": 1, \"Items\": [ \"/*\" ] }, \"CallerReference\": \"$(shell date +%s)\" }"
+#aws --profile uneet-prod cloudfront create-invalidation --distribution-id E3NBG008M01XS8 --invalidation-batch "{ \"Paths\": { \"Quantity\": 1, \"Items\": [ \"/*\" ] }, \"CallerReference\": \"$(shell date +%s)\" }"
